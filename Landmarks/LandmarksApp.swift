cb8060d519e89cc0111eb2617c152a3e569d5c8a
rozran00@gmail.com <rozran00@gmail.com>
2021-05-14 13:50:27 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jacob Rozran on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
