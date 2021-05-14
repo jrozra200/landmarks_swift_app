7ac2fd59411e7e40a3844ab2e1acf60148fe9531
rozran00@gmail.com <rozran00@gmail.com>
2021-05-14 14:53:08 -0400

    first commit

--- Landmarks/CreatingAndCombiningViews/Complete/Landmarks/ContentView.swift
+++ Landmarks/CreatingAndCombiningViews/Complete/Landmarks/ContentView.swift
@@ -1,9 +1,9 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Jacob Rozran on 5/14/21.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
 
 import SwiftUI
 
@@ -13,32 +13,31 @@ struct ContentView: View {
             MapView()
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
-            
+
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
-        
+
             VStack(alignment: .leading) {
-                            
                 Text("Turtle Rock")
                     .font(.title)
+
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                     Spacer()
                     Text("California")
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
-                
+
                 Divider()
-                
+
                 Text("About Turtle Rock")
                     .font(.title2)
-                Text("Description text goes here")
+                Text("Descriptive text goes here.")
             }
             .padding()
-            
+
             Spacer()
         }
     }
